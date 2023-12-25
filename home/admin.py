a7986658cea6bc009eb747940a1ff6f3f64e3218
grace-ghb <>
2023-12-25 00:36:35 +0000

    Install summernote app

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Post
 # import Post from models.py class Post
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
