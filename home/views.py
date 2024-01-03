54fef85efa4eb8ef44d368a17db64caecd8190f0
grace-ghb <>
2024-01-03 21:05:08 +0000

    Deploy Heroku, remove 'about' app

--- home/views.py
+++ home/views.py
@@ -3,7 +3,11 @@ from django.views import generic
 from django.views.generic import TemplateView
 from .models import Post
 
+
 class PostList(generic.ListView):
+    """
+    This is for the Home page
+    """
     model = Post
     queryset = Post.objects.filter(status=1).order_by('created_on')
     template_name = 'index.html'
